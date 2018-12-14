ed68db2acb6e2732fd45d3e61bc641fda4cedc8e
SharadDixit <sdixit1@umbc.edu>
2018-12-13 22:10:23 -0500

    check

--- ROI/RoICrop.py
+++
@@ -1,6 +0,0 @@
-from torch.nn.modules.module import Module
-
-
-class RoICrop(Module):
-    def __init__(self, layout = 'BHWD'):
-        super(RoICrop, self).__init__()
--- ROI/RoIPooling.py
+++
@@ -1,9 +0,0 @@
-from torch.nn.modules.module import Module
-
-class RoIPooling(Module):
-    def __init__(self, pooledHeight, pooledWidth, spatialScale):
-        super(RoIPooling, self).__init__()
-
-        self.pooled_width = int(pooledWidth)
-        self.pooled_height = int(pooledHeight)
-        self.spatial_scale = float(spatialScale)
